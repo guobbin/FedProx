6b8f08ea30102b87f0670d3b4b601c2fad57be70
guobbin <1521381396@qq.com>
2020-06-21 17:58:21 +0800

    cnn+512+drop

--- flearn/models/mnist/mclr.py
+++ flearn/models/mnist/mclr.py
@@ -21,7 +21,7 @@ class Model(object):
         self.graph = tf.Graph()
         with self.graph.as_default():
             tf.set_random_seed(123+seed)
-            self.features, self.labels, self.train_op, self.grads, self.eval_metric_ops, self.loss = self.create_model(optimizer)
+            self.features, self.labels, self.train_op, self.grads, self.eval_metric_ops, self.loss, self.keep_prob1, self.keep_prob2 = self.create_model(optimizer)
             self.saver = tf.train.Saver()
         self.sess = tf.Session(graph=self.graph)
 
@@ -54,19 +54,24 @@ class Model(object):
             activation=tf.nn.relu)
         pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
         pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-        dense = tf.layers.dense(inputs=pool2_flat, units=512, activation=tf.nn.relu)
-        logits = tf.layers.dense(inputs=dense, units=self.num_classes)
+
+        h_fc1 = tf.layers.dense(inputs=pool2_flat, units=512, activation=tf.nn.relu)
+        keep_prob1 = tf.placeholder("float")
+        h_fc1_drop1 = tf.nn.dropout(h_fc1, keep_prob1)
+        h_fc2 = tf.layers.dense(inputs=h_fc1_drop1, units=self.num_classes)
+        keep_prob2 = tf.placeholder("float")
+        h_fc1_drop2 = tf.nn.dropout(h_fc2, keep_prob2)
         predictions = {
-            "classes": tf.argmax(input=logits, axis=1),
-            "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+            "classes": tf.argmax(input=h_fc1_drop2, axis=1),
+            "probabilities": tf.nn.softmax(h_fc1_drop2, name="softmax_tensor")
             }
-        loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+        loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=h_fc2)
 
         grads_and_vars = optimizer.compute_gradients(loss)
         grads, _ = zip(*grads_and_vars)
         train_op = optimizer.apply_gradients(grads_and_vars, global_step=tf.train.get_global_step())
         eval_metric_ops = tf.count_nonzero(tf.equal(labels, predictions["classes"]))
-        return features, labels, train_op, grads, eval_metric_ops, loss
+        return features, labels, train_op, grads, eval_metric_ops, loss, keep_prob1, keep_prob2
 
     def set_params(self, model_params=None):
         if model_params is not None:
@@ -87,7 +92,7 @@ class Model(object):
 
         with self.graph.as_default():
             model_grads = self.sess.run(self.grads,
-                feed_dict={self.features: data['x'], self.labels: data['y']})
+                feed_dict={self.features: data['x'], self.labels: data['y'], self.keep_prob1: 1, self.keep_prob2: 1})
             grads = process_grad(model_grads)
 
         return num_samples, grads
@@ -98,7 +103,7 @@ class Model(object):
             for X, y in batch_data(data, batch_size):
                 with self.graph.as_default():
                     self.sess.run(self.train_op,
-                        feed_dict={self.features: X, self.labels: y})
+                        feed_dict={self.features: X, self.labels: y, self.keep_prob1: 0.25, self.keep_prob2: 0.5})
         soln = self.get_params()
         comp = num_epochs * (len(data['y'])//batch_size) * batch_size * self.flops
         return soln, comp
@@ -108,7 +113,7 @@ class Model(object):
 
         for X, y in batch_data_multiple_iters(data, batch_size, num_iters):
             with self.graph.as_default():
-                self.sess.run(self.train_op, feed_dict={self.features: X, self.labels: y})
+                self.sess.run(self.train_op, feed_dict={self.features: X, self.labels: y, self.keep_prob1: 0.25, self.keep_prob2: 0.5})
         soln = self.get_params()
         comp = 0
         return soln, comp
@@ -120,7 +125,7 @@ class Model(object):
         '''
         with self.graph.as_default():
             tot_correct, loss = self.sess.run([self.eval_metric_ops, self.loss], 
-                feed_dict={self.features: data['x'], self.labels: data['y']})
+                feed_dict={self.features: data['x'], self.labels: data['y'], self.keep_prob1: 1, self.keep_prob2: 1})
         return tot_correct, loss
     
     def close(self):
